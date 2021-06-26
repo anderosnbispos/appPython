70df1f54b54e4510df39bb0e104053421cbf87a6
andersonbispos <andersonbispos@gmail.com>
2021-06-26 09:07:35 -0300

    Aula 5

---
+++ codes/aula5.py
@@ -0,0 +1,62 @@
+lista = [1, 10, 7, 3]
+animal = ['cachorro','gato','elefante','lobo','arara']
+
+tupla = (1, 10, 12, 14)
+
+tanimal = tuple(animal)
+print(animal,type(animal))
+print(tanimal,type(tanimal))
+
+ltupla = list(tupla)
+print(tupla,type(tupla))
+print(ltupla,type(ltupla))
+
+
+# animal[0] = 'macaco'
+# print(animal)
+# print(len(animal))
+#
+#
+# print(tupla[2])
+# print(len(tupla))
+
+# lista.sort()
+# animal.sort()
+# print(lista)
+# print(animal)
+#
+# lista.reverse()
+# animal.reverse()
+# print(lista)
+# print(animal)
+
+# newanimal = animal * 3
+# print(newanimal)
+
+# if 'veado' in animal:
+#     print('sim')
+# else:
+#     print('nao')
+#     animal.append('veado')
+#     print(animal)
+#
+# animal.pop()
+# print(animal)
+
+# print(sum(lista))
+# print(min(lista))
+# print(max(lista))
+#
+# print(max(animal))
+# print(lista,type(lista))
+# print(animal,type(lista))
+
+# soma = 0
+# for x in lista:
+#     print(x)
+#     soma += x
+
+
+
+# for x in animal:
+#     print(x)
